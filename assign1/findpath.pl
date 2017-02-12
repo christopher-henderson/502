a8c725f4449b6d3b59db8d060c9d02a414fa65b3
christopher-henderson <chris@chenderson.org>
2017-02-12 09:15:50 -0700

    moving names around

--- assign1/findpath.pl
+++ assign1/findpath.pl
@@ -1,37 +1,34 @@
-findpath(a, b, 1, [a, b]).
-findpath(a, c, 6, [a, c]).
+edge(a, b, 1).
+edge(a, c, 6).
+edge(b, a, 1).
+edge(b, e, 1).
+edge(b, d, 3).
+edge(b, c, 4).
+edge(e, b, 1).
+edge(e, d, 1).
+edge(e, j, 2).
+edge(d, e, 1).
+edge(d, b, 3).
+edge(d, c, 1).
+edge(c, d, 1).
+edge(c, b, 4).
+edge(c, a, 6).
 
-findpath(b, a, 1, [b, a]).
-findpath(b, e, 1, [b, e]).
-findpath(b, d, 3, [b, d]).
-findpath(b, c, 4, [b, c]).
+% Wrap the outer call with our own list to keep track of visited nodes.
+findpath(X, Y, W, Path) :- findpath(X, Y, W, Path, [a,b,c,d,e,j]).
+% If there is a direct edge, then that is the answer. Base case.
+findpath(X, Y, W, Path, _) :- edge(X, Y, W), Path = [X, Y].
+% The recursive solution.
+findpath(X, Y, W, Path, NotVisited) :-
+                 X \= Y,                         % The source and the destination CANNOT be the same.
+                                                 %    If there is something like A --> A in the graph, then
+                                                 %    the above predicate will find it out.
+                 edge(Z, Y, W1),                 % Find some Z the definitely has an edge with Y.
+                 member(Z, NotVisited),          % Z Must not have been visited yet, lest that be a cycle.
+                 select(Y, NotVisited, NV1),     % Mark Y as having been visited. Do not mark Z yet as we
+                                                 %    have not found that path yet.
+                 findpath(X, Z, W2, Path1, NV1), % Find a path from X to Z.
+                 appendToTail(Path1, Y, Path),   % The final path is the path from X to Z plus Y.
+                 W is W1 + W2.                   % Calculate final weight.
 
-findpath(e, b, 1, [e, b]).
-findpath(e, d, 1, [e, d]).
-findpath(e, j, 2, [e, j]).
-
-findpath(d, e, 1, [d, e]).
-findpath(d, b, 3, [d, b]).
-findpath(d, c, 1, [d, c]).
-
-findpath(c, d, 1, [c, d]).
-findpath(c, b, 4, [c, b]).
-findpath(c, a, 6, [c, b]).
-
-
-%findpath(H, Y, _, [H|T]) :- \+ cycle([H|T]), last(Y, T).
-
-findpath(X,Y,Weight,Path) :-
-                             write("Path so far: "), write(Path),
-                             findpath(Z, Y, W1, P1),
-                             write("Found path: "), write(P1),
-                             findpath(X, Z, W2, P2),
-                             write("Found path2: "), write(P2),
-                             appendTail(P2, P1, Path),
-                             write("Path is: "), write(Path),
-                             Weight is W1 + W2.
-
-noCycle(Path) :- list_to_set(Path) =\= Path;
-%noCycle([]).
-%noCycle([H|T]) :- \+ member(H, T).
-appendTail(A, [_|T], Result) :- append(A, T, Result).
+appendToTail(List, Element, Result) :- flatten([List, Element], Result).
