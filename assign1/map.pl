0aa926e88dcb66fed0d11090b4b6b79f116ecad0
christopher-henderson <chris@chenderson.org>
2017-02-13 16:48:29 -0700

    map coloring works

--- assign1/map.pl
+++ assign1/map.pl
@@ -17,3 +17,48 @@ L = [[1, red], [2, green], [3, blue], [4, yellow], [5, red], [6, green]];
 …
 …
 */
+
+edge(1, 2).
+edge(1, 3).
+edge(1, 6).
+edge(1, 4).
+
+edge(2, 1).
+edge(2, 3).
+edge(2, 5).
+
+edge(3, 2).
+edge(3, 1).
+edge(3, 6).
+edge(3, 4).
+
+edge(4, 1).
+edge(4, 3).
+edge(4, 5).
+edge(4, 6).
+
+edge(5, 2).
+edge(5, 3).
+edge(5, 4).
+
+edge(6, 3).
+edge(6, 1).
+edge(6, 4).
+
+color_map(L) :- color(1, [[1, null], [2, null], [3, null], [4, null], [5, null], [6, null]], [red, green, blue, yellow], L).
+
+color(Vertex, Map, Colors, L) :-  Vertex < 7,
+                               member(Color, Colors),
+                               maplist(validColor(Vertex, Color), Map),
+                               V2 is Vertex + 1,
+                               updateMap(Vertex, Color, Map, Map2),
+                               color(V2, Map2, Colors, L).
+
+color(_, Map, _, L) :-  L = Map.
+
+validColor(X, C1, [Y, C2]) :- \+ edge(X, Y);
+                             X \= Y,
+                             C1 \= C2.
+
+updateMap(Vertex, Color, Map, Map2) :- select([Vertex, null], Map, M),
+                                      append(M, [[Vertex, Color]], Map2).
