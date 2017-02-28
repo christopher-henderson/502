1c6f0afd707ed7473846ebce352891bec691b71b
christopher-henderson <chris@chenderson.org>
2017-02-28 12:36:47 -0700

    kinda done with sudoku

--- assign2/two.pl
+++ assign2/two.pl
@@ -26,16 +26,51 @@ Sudoku
 
 sudoku(Puzzle) :- length(Puzzle, 9),
                   setRows(Puzzle),
-                  transpose(Puzzle, Puzzle2),
-                  setRows(Puzzle2),
-                  flatten(Puzzle, P2),
-                  label(P2).
-                  %label(Puzzle).
+                  transpose(Puzzle, Columns),
+                  setRows(Columns),
+                  setGrids(Puzzle, 0),
+                  flatten(Puzzle, SolvedPuzzle),
+                  label(SolvedPuzzle),
+                  write(Puzzle).
 
 setRows([]).
-setRows([Row|Others]) :- Row ins 1..9,
+setRows([Row|Others]) :- length(Row, 9),
+                         Row ins 1..9,
                          all_distinct(Row),
                          setRows(Others).
+
+/*
+Applies the all_distinct constraint to all 3x3 subgrids.
+*/
+setGrids(Grid, N) :- length(Grid, L), N #>= L.
+setGrids(Grid, N1) :- N2 #= N1 + 1,
+                      N3 #= N1 + 2,
+                      N4 #= N1 + 3,
+                     nth0(N1, Grid, Row1),
+                     nth0(N2, Grid, Row2),
+                     nth0(N3, Grid, Row3),
+                     slice(Row1, N1, N4, SlicedRow1),
+                     slice(Row2, N1, N4, SlicedRow2),
+                     slice(Row3, N1, N4, SlicedRow3),
+                     append(SlicedRow1, SlicedRow2, SubGrid),
+                     append(SubGrid, SlicedRow3, FinalSubGrid),
+                     all_distinct(FinalSubGrid),
+                     NextSubGrid #= N4,
+                     setGrids(Grid, NextSubGrid).
+
+/*
+This is the most convenient way I could think of extracting the sub grids
+necessary for applying the all_distinct contraint to each 3x3 grid.
+*/
+slice(List, Beginning, End, Result) :- slice(List, Beginning, End, Result, []).
+slice(_, End, End, Result, Acc) :- Result = Acc.
+slice(List, Beginning, _, Result, Acc) :- length(List, L),
+                                            L #=< Beginning,
+                                            Result = Acc.
+slice(List, Beginning, End, Result, Acc) :- nth0(Beginning, List, Value),
+                                            append(Acc, [Value], Acc2),
+                                            Beginning2 #= Beginning + 1,
+                                            slice(List, Beginning2, End, Result, Acc2).
 
 problem(1, [[_,_,6, 5,9,_, _,_,_],
  [_,_,3, _,_,_, _,7,_],
@@ -46,5 +81,3 @@ problem(1, [[_,_,6, 5,9,_, _,_,_],
  [2,_,_, _,_,5, _,_,8],
  [_,3,8, _,_,1, _,_,_],
  [_,_,_, 3,_,_, 7,5,4]]).
-
- color_map(L)
